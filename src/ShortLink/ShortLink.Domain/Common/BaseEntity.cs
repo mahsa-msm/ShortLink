10a0dd3e31091b03bdff810cc5584e9c6b902600
mahsa mousavi <mahsaa.mousavi77@gmail.com>
2022-02-22 10:12:30 +0330

    added base entity and link entity in domain

---
+++ src/ShortLink/ShortLink.Domain/Common/BaseEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShortLink.Domain
+{
+    public class BaseEntity<Ikey>
+    {
+        public Ikey Id { get; set; }
+    }
+}
---
+++ src/ShortLink/ShortLink.Domain/Links/Link.cs
@@ -0,0 +1,9 @@
+﻿namespace ShortLink.Domain.Links
+{
+    public class Link : BaseEntity<int>
+    {
+        public string MainLink { get; set; }
+        public string ShortLink { get; set; }
+        public long VisitCount { get; set; }
+    }
+}
