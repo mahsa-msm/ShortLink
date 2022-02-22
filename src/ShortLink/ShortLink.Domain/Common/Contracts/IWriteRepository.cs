5fef78604d0c6b345f745c7d10c259a20bfec3ac
mahsa mousavi <mahsaa.mousavi77@gmail.com>
2022-02-22 10:43:19 +0330

    added base repository for read and write command

---
+++ src/ShortLink/ShortLink.Domain/Common/Contracts/IWriteRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+
+namespace ShortLink.Domain
+{
+    public interface IWriteRepository<TEntity> where TEntity : BaseEntity<int>
+    {
+        Task CreateAsync(TEntity entity);
+        void UpdateAsync(TEntity entity);
+        void DeleteAsync(TEntity entity);
+        Task SaveChangeAsync();
+    }
+}
---
+++ src/ShortLink/ShortLink.EfCore/Repositories/Common/WriteRepository.cs
@@ -0,0 +1,34 @@
+﻿using ShortLink.Domain;
+using System.Threading.Tasks;
+
+namespace ShortLink.EfCore.Repositories.Common
+{
+    public class WriteRepository<TEntity> : IWriteRepository<TEntity> where TEntity : BaseEntity<int>
+    {
+        private readonly ApplicationDbContext _context;
+        public WriteRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task CreateAsync(TEntity entity)
+        {
+            await _context.AddAsync(entity);
+        }
+
+        public void DeleteAsync(TEntity entity)
+        {
+            _context.Remove(entity);
+        }
+
+        public void UpdateAsync(TEntity entity)
+        {
+            _context.Update(entity);
+        }
+
+        public async Task SaveChangeAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
