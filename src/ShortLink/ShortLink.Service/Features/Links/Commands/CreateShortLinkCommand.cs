296df2835647d8464ffcef65bc23e15dbb68a96e
mahsa mousavi <mahsaa.mousavi77@gmail.com>
2022-02-22 11:53:04 +0330

    added mediator and implimented  a new command for create short link

---
+++ src/ShortLink/ShortLink.Service/Features/Links/Commands/CreateShortLinkCommand.cs
@@ -0,0 +1,36 @@
+﻿using MediatR;
+using ShortLink.Domain;
+using ShortLink.Domain.Links;
+using ShortLink.Service.Features.Links.Dtos;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ShortLink.Service.Features.Links.Commands
+{
+    public partial class CreateShortLinkCommand : IRequest<ShortLinkDto>
+    {
+        public string MainLink { get; set; }
+
+        private class Handler : IRequestHandler<CreateShortLinkCommand, ShortLinkDto>
+        {
+            private readonly IWriteRepository<Link> _linkRepository;
+            private readonly IMediator _mediator;
+            public Handler(IWriteRepository<Link> linkRepository, IMediator mediator)
+            {
+                _linkRepository = linkRepository;
+                _mediator = mediator;
+            }
+
+            public async Task<ShortLinkDto> Handle(CreateShortLinkCommand command, CancellationToken cancellationToken)
+            {
+                var shortLink = await _mediator.Send(new GeneratNewShortLinkCommand());
+
+                var link = new Link(command.MainLink, shortLink);
+                await _linkRepository.CreateAsync(link);
+                await _linkRepository.SaveChangeAsync();
+
+                return new ShortLinkDto { ShortLink = shortLink};
+            }
+        }
+    }
+}
---
+++ src/ShortLink/ShortLink.Service/Features/Links/Commands/GeneratNewShortLinkCommand.cs
@@ -0,0 +1,38 @@
+﻿using MediatR;
+using ShortLink.Domain;
+using ShortLink.Domain.Links;
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ShortLink.Service.Features.Links.Commands
+{
+    public partial class GeneratNewShortLinkCommand : IRequest<string>
+    {
+        private class Handler : IRequestHandler<GeneratNewShortLinkCommand, string>
+        {
+            private readonly IReadRepository<Link> _linkRepository;
+            private readonly IMediator _mediator;
+
+            public Handler(IReadRepository<Link> linkRepository, IMediator mediator)
+            {
+                _linkRepository = linkRepository;
+                _mediator = mediator;
+            }
+
+            public async Task<string> Handle(GeneratNewShortLinkCommand command, CancellationToken cancellationToken)
+            {
+                Random random = new Random();
+                const string characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+                var shortLink = new string(Enumerable.Repeat(characters, 10).Select(s => s[random.Next(s.Length)]).ToArray());
+
+                if (_linkRepository.TableNoTracking.Any(c => c.ShortLink == shortLink))
+                {
+                    shortLink = await _mediator.Send(new GeneratNewShortLinkCommand());
+                }
+                return shortLink;
+            }
+        }
+    }
+}
