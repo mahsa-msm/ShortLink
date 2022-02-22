4f901dc839bb7a32195c07de9700f7a2bbd774c5
mahsa mousavi <mahsaa.mousavi77@gmail.com>
2022-02-22 10:26:31 +0330

    added  application db context and added migration for link

---
+++ src/ShortLink/ShortLink.EfCore/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShortLink.Domain.Links;
+
+namespace ShortLink.EfCore
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Link> Links { get; set; }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ src/ShortLink/ShortLink.EfCore/Migrations/20220222065514_CreateLinkEntity.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ShortLink.EfCore.Migrations
+{
+    public partial class CreateLinkEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Links",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    MainLink = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ShortLink = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    VisitCount = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Links", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Links");
+        }
+    }
+}
