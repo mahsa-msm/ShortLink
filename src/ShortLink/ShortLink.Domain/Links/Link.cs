98db404fa21f12e280b25c4f7a48a1f91df2cdcf
mahsa mousavi <mahsaa.mousavi77@gmail.com>
2022-02-22 10:26:06 +0330

    changed link entity

--- src/ShortLink/ShortLink.Domain/Links/Link.cs
+++ src/ShortLink/ShortLink.Domain/Links/Link.cs
@@ -2,8 +2,20 @@
 {
     public class Link : BaseEntity<int>
     {
-        public string MainLink { get; set; }
-        public string ShortLink { get; set; }
-        public long VisitCount { get; set; }
+        public string MainLink { get; private set; }
+        public string ShortLink { get; private set; }
+        public long VisitCount { get; private set; }
+
+        private Link()
+        {
+
+        }
+
+        public Link(string mainLink, string shortLink)
+        {
+            MainLink = mainLink;
+            ShortLink = shortLink;
+            VisitCount = 0;
+        }
     }
 }
