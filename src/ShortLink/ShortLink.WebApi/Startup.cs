ce525029ea7807a209d8d73e3f63b8ee96a13a57
mahsa mousavi <mahsaa.mousavi77@gmail.com>
2022-02-22 10:26:54 +0330

    configured start up

--- src/ShortLink/ShortLink.WebApi/Startup.cs
+++ src/ShortLink/ShortLink.WebApi/Startup.cs
@@ -2,11 +2,13 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using ShortLink.EfCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -32,6 +34,12 @@ namespace ShortLink.WebApi
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "ShortLink.WebApi", Version = "v1" });
             });
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("ApplicationDbContext"));
+            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
